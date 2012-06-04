16c651dc02c5620d0644f94feb0b4157acd861cd
HATTORI, Hiroki <hattori@computron.co.jp>
2012-06-04 13:52:18 +0900

    節のマージ関数を追加

--- SAT/ToySAT/Types.hs
+++ SAT/ToySAT/Types.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ViewPatterns,OverloadedStrings,MagicHash #-}
 module SAT.ToySAT.Types (CNF(..),Clause,Lit(..),neg,unLit,isPos) where
 
+import Control.Applicative ((<$>))
 import Data.List (intersperse)
 import Data.Function (on)
 
@@ -28,6 +29,29 @@ neg = Lit . negate . unLit
 type Clause = [Lit]
 
 --compareClause :: Clause -> Clause -> Ordering
+
+{- | 2つの節をマージします。
+
+  1. 各々の節はソート済みで、かつ矛盾していない必要がある
+  2. 互いに矛盾していたらNothingになります
+
+>>> merge [Lit 1,Lit 2,Lit (-3)] [Lit 2,Lit 4,Lit 5,Lit 6]
+Just [1,2,-3,4,5,6]
+
+>>> merge [Lit 1,Lit 2,Lit 3] [Lit (-2),Lit 4,Lit 5,Lit 6]
+Nothing
+
+ -}
+merge :: Clause -> Clause -> Maybe Clause
+merge [] ys = Just ys
+merge xs [] = Just xs
+merge xss@(x:xs) yss@(y:ys)
+  | x == neg y = Nothing
+  | x == y     = (x:) <$> merge xs ys 
+  | x < y      = (x:) <$> merge xs yss
+  | otherwise  = (y:) <$> merge xss ys
+
+
 
 data CNF = CNF Int [Clause]
 instance Show CNF where
